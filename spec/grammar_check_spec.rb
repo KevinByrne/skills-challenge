4e381b90bd0bd08d4f8d944f5dad362f77be4b6f
KevinByrne <kevbee79@gmail.com>
2022-06-24 11:43:44 +0100

    first test fail, no method

--- spec/grammar_check_spec.rb
+++ spec/grammar_check_spec.rb
@@ -1,2 +1,10 @@
 require 'grammar_check'
 
+RSpec.describe "Checks the grammar in a text and returns true or false if text is made up of capitalized sentences and appropriate end punctuation marks." do
+
+  it 'accepts a body of text as an argument' do
+    result = grammar_check("this is a sentence")
+    expect(result).to eq 'this is a sentence'
+  end
+  
+end
