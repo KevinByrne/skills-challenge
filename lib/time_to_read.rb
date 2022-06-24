f438908295827be39bed581aaa26ed34d5c4bb48
KevinByrne <kevbee79@gmail.com>
2022-06-24 10:52:28 +0100

    last test failing for amount of words being less than words per minute

--- lib/time_to_read.rb
+++ lib/time_to_read.rb
@@ -3,5 +3,7 @@ def reading_time(amount_of_words)
   est_reading_time = amount_of_words / words_per_minute
   if amount_of_words >= words_per_minute
     return "This will take approx. #{est_reading_time} minute(s) to read."
+  else amount_of_words < words_per_minute
+    return "This will take less than 1 minute to read"
   end
 end
--- spec/time_to_read_spec.rb
+++ spec/time_to_read_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe "estimates reading time based on word per minute" do
 
   it "returns an estimated reading time of two minutes based on 200 words per minute" do
     result = reading_time(400)
-    expect(result).to eq 2
+    expect(result).to eq "This will take approx. 2 minute(s) to read."
   end
 
   it "returns an estimated reading time of three minutes based on 200 words per minute" do
     result = reading_time(600)
-    expect(result).to eq 3
+    expect(result).to eq "This will take approx. 3 minute(s) to read."
   end
 
   it "returns an estimated reading time of less than a minute based on 200 words per minute" do
     result = reading_time(150)
-    expect(result).to eq 0
+    expect(result).to eq 
   end
 end
