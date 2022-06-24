365c1108d90067c3d4f47da89b706cdf438bef09
KevinByrne <kevbee79@gmail.com>
2022-06-24 10:48:40 +0100

    first pass of test post refactor

--- lib/time_to_read.rb
+++ lib/time_to_read.rb
@@ -1,4 +1,7 @@
 def reading_time(amount_of_words)
   words_per_minute = 200
   est_reading_time = amount_of_words / words_per_minute
+  if amount_of_words >= words_per_minute
+    return "This will take approx. #{est_reading_time} minute(s) to read."
+  end
 end
--- spec/time_to_read_spec.rb
+++ spec/time_to_read_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "estimates reading time based on word per minute" do
   
   it "returns an esitmated reading time of one minute based on 200 words per minute"do
     result = reading_time(200)
-    expect(result).to eq "This will approx. 1 minute(s) to read."
+    expect(result).to eq "This will take approx. 1 minute(s) to read."
   end
 
   it "returns an estimated reading time of two minutes based on 200 words per minute" do
